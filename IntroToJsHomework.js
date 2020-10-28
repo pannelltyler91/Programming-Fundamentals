18f485e84a530f1474635ef92692f0c3fa2666ad
Tyler Pannell <pannelltyler91@gmail.com>
2020-10-28 19:20:02 -0400

    added final touches to homework for IntroJS

--- IntroToJsHomework.js
+++ IntroToJsHomework.js
@@ -35,3 +35,43 @@ function hello(name){
             
             tipAmount (100,"fair");
             tipAmount (500,"good");
+
+
+            function totalAmount (amount, service){
+            var bad=("bad")
+            var fair=("fair")
+            var good=("good")
+            if (service === bad) 
+            console.log((amount * 0.10) + amount)
+            else if(service === fair)
+            console.log((amount * 0.15) + amount)
+            else if(service === good)
+            console.log((amount * 0.20) + amount)
+            else 
+        console.log ("No tip! Customer service almost non-existent")
+        }
+
+totalAmount(100,"good");
+
+
+function splitAmount(amount, service, partySize){
+    var bad=("bad")
+    var fair=("fair")
+    var good=("good")
+    if(service === bad)
+    console.log(((amount * 0.10) + amount) / partySize)
+    else if (service === fair) 
+    console.log(((amount * 0.15) + amount) / partySize)
+    else if (service === good)
+    console.log(((amount * 0.20) + amount) / partySize)
+    else 
+    console.log("Not tip! Customer service almost non-existent")
+    }
+  
+    splitAmount(100,"bad",3);
+     let number=(0)
+    for (let number = 0; number <= 10; number = number + 1) {
+        console.log(number);
+      }
+
+  number;
