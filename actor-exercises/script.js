137e6d1cc3dc6d23e54b85c3640e848999b76335
Tyler Pannell <pannelltyler91@gmail.com>
2020-10-30 00:08:44 -0400

    adding exploringJs.js and actor-exercises/script.js

---
+++ actor-exercises/script.js
@@ -0,0 +1,26 @@
+var actorOne = {
+    firstName: "Will",
+    lastName: "Smith"
+    age: 51,
+    yearlyEarnings: "1.2 million"
+
+}
+
+var actorTwo = {
+    firstName: "Tom",
+    lastName: "Cruise",
+    age: 55,
+    yearlyEarnings: "3.2 million",
+}
+
+var actorThree = {
+    firstName: "Selma",
+    lastName: "Hayek",
+    age: 50, 
+    yearlyEarnings: "1 million", 
+}
+
+var actorArray = [actorOne,actorTwo,actorThree]
+
+
+}
---
+++ exploringJs.js
@@ -0,0 +1,11 @@
+
+for (let number=1; number <= 10; number ++) {
+  
+    console.log(number);
+  }
+
+let number = 1;
+while(number <= 10) {
+    console.log(number);
+    number++;
+}
