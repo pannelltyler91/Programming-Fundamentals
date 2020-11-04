8723398c3e304e84e9bec6e4a134f817917bba3a
Tyler Pannell <pannelltyler91@gmail.com>
2020-11-04 02:00:20 -0500

    pushing script.js and JsAsyncCallback.js

--- JsAsyncCallback.js
+++ JsAsyncCallback.js
@@ -1,6 +1,10 @@
 
 let numberArray = [1,2,3,4,5,6,7,8,9,]
+let evenNumberArray = []
+for (let i = 0; i < numberArray.length; i++) {
+    if( numberArray[i] % 2 === 0)
+    console.log (numberArray[i]);
 
-function positiveNumbers = (num1,num2){
 
 }
+
---
+++ script.js
@@ -0,0 +1,18 @@
+ let star = "*"
+ 
+ 
+ 
+ function printSquare(number){
+for (let i = 1; i < number; i++){
+    for (let j = 1; j < number; j++){
+        if(i |j  == number)
+            console.log(star)
+        
+        
+
+    }  
+}
+ }
+
+ printSquare(5);
+ 
